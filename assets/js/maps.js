8cf638f36fb6ba2aba0bb49bae7e93f5809d7b67
mosull20 <mosullivan@mac.com>
2021-04-17 18:34:22 +0000

    added google maps api

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map-one"), {
+        zoom: 5,
+        center: {
+            lat: 64.963942,
+            lng: -19.021169
+        }
+    });
+}
