ae2387f9c1f7c2a1be431eaa961975604b2c798f
mosull20 <mosullivan@mac.com>
2021-04-18 15:13:16 +0000

    added click function to hide/show div when country selected

--- assets/js/destinations.js
+++ assets/js/destinations.js
@@ -1,17 +1,48 @@
-// function to toggle font awesome arrow up or down on click
-// assisted by Stack overflow 
-function arrowSwitch(event) {
-    this.classList.toggle("fa-angle-double-up")
-}
+// function to display the relevant div on clicking 
 
-let arrow = document.getElementById('arrow');
-arrow.addEventListener('click', arrowSwitch);
+$(document).ready(function () {
+    $("#arrow-iceland").click(function () {
+        $("#display-country-details-iceland").show();
+        $("#display-country-details-malta").hide();
+        $("#display-country-details-newzealand").hide();
+        $("#display-country-details-morocco").hide();
+        $("#display-country-details-ireland").hide();
 
-// $(".iceland-text a").click(function () {
-//     $("#box-one").html(
-// )
-// });
+    });
+    $("#arrow-malta").click(function () {
+        $("#display-country-details-malta").show();
+        $("#display-country-details-iceland").hide();
+        $("#display-country-details-newzealand").hide();
+        $("#display-country-details-morocco").hide();
+        $("#display-country-details-ireland").hide();
+    });
+    $("#arrow-newzealand").click(function () {
+        $("#display-country-details-newzealand").show();
+        $("#display-country-details-iceland").hide();
+        $("#display-country-details-malta").hide();
+        $("#display-country-details-morocco").hide();
+        $("#display-country-details-ireland").hide();
+    });
+    $("#arrow-morocco").click(function () {
+        $("#display-country-details-morocco").show();
+        $("#display-country-details-iceland").hide();
+        $("#display-country-details-malta").hide();
+        $("#display-country-details-newzealand").hide();
+        $("#display-country-details-ireland").hide();
 
+    });
+    $("#arrow-ireland").click(function () {
+        $("#display-country-details-ireland").show();
+        $("#display-country-details-iceland").hide();
+        $("#display-country-details-malta").hide();
+        $("#display-country-details-newzealand").hide();
+        $("#display-country-details-morocco").hide();
+
+    });
+
+});
+
+// function to push html depending on which heading the user clicks on
 $("#iceland-li-one").click(function () {
     $("#iceland-div").html(
         `<p>Dettifoss (Falling Waterfall) is an amazing waterfall. No wonder Riddley Scott chose this
@@ -35,6 +66,34 @@ $("#iceland-li-two").click(function () {
     $("#iceland-div").html(
         `<p>The black sandy beaches that appear in scenes throughout Rogue One: A Star Wars story can be found in Mýrdalssandur. 
         The black sands are formed from lava. And despite the barren landscape, arctic foxes and seabirds can often be found here.
+        </p>
+        `
+    );
+})
+
+$("#iceland-li-three").click(function () {
+    $("#iceland-div").html(
+        `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam fringilla lectus elementum maximus. 
+        Praesent orci mauris, consequat eu nisl nec, mollis efficitur nunc. Cras vestibulum varius congue. Nullam in metus 
+        vitae dui efficitur tempor.
+        </p>
+        `
+    );
+})
+
+$("#iceland-li-four").click(function () {
+    $("#iceland-div").html(
+        `<p>Fusce pulvinar ante ut orci consectetur luctus. Donec ultrices dolor sit amet malesuada pharetra. 
+        Proin fringilla magna sit amet odio vestibulum, sed fermentum mi vulputate. Sed urna massa, tincidunt at convallis eu, viverra a metus.
+        </p>
+        `
+    );
+})
+
+$("#iceland-li-five").click(function () {
+    $("#iceland-div").html(
+        `<p>Morbi ut eleifend lorem, sit amet lobortis metus. Aenean eu elit ac risus vestibulum convallis. 
+        Vestibulum cursus turpis sed auctor sodales. Nunc neque diam, elementum sit amet neque in, volutpat bibendum lectus.
         </p>
         `
     );
