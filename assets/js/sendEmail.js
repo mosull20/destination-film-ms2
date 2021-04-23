7776bf983c7f7360c547468780e32802b9d2a0d2
mosull20 <mosullivan@mac.com>
2021-04-23 16:40:51 +0000

    added modal to submit on contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -6,9 +6,24 @@ function sendMail(contactForm) {
         })
         .then(
             function (response) {
+                let successHeading = document.getElementById('exampleModalLabel');
+                successHeading.innerHTML = `Success!`
+                let successMessage = document.getElementById('myModalText');
+                successMessage.innerHTML =
+                    `<p>Your message has been sent. Thank you for getting in touch!</p>
+                    `
+                $('#exampleModal').modal('show');
                 console.log("Success!", response);
+                document.getElementById('my-contact-form').reset();
             },
             function (error) {
+                let successHeading = document.getElementById('exampleModalLabel');
+                successHeading.innerHTML = `Oops!`
+                let successMessage = document.getElementById('myModalText');
+                successMessage.innerHTML =
+                    `<p>Something went wrong, please try again later!</p>
+                    `
+                $('#exampleModal').modal('show')
                 console.log("Failed!", error);
             });
     return false;
